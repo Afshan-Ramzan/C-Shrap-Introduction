107c986e9abe365dcce398e49550ada6eb94a381
afshan ramzan <afshan_ramzan@hotmail.com>
2020-03-20 13:14:23 +0500

    Using namespaces

---
+++ ProjectA.TeamB/ClassA.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectA.TeamB
+{
+    public class ClassA
+    {
+        public static void print()
+        {
+            Console.WriteLine("Team b print");
+        }
+    }
+}
