57b2713c4c827323cacb46d313e6fbd8270ad3a5
afshan ramzan <afshan_ramzan@hotmail.com>
2020-03-20 17:08:00 +0500

    inner Exception handling with example

---
+++ 18-innerException/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace _18_innerException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                try
+                {
+                    Console.WriteLine("Enter First number ");
+                    int FN = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter Second number ");
+                    int SN = Convert.ToInt32(Console.ReadLine());
+                    int result = FN / SN;
+                    Console.WriteLine("Result of division is {0}", result);
+                }
+                catch (Exception ex)
+                {
+                    string filepath = @"C\log.txt";
+                    if (File.Exists(filepath))
+                    {
+                        StreamWriter sw = new StreamWriter(filepath);
+                        sw.Write(ex.GetType().Name);
+                        sw.WriteLine();
+                        sw.Write(ex.Message);
+                        sw.Close();
+                        Console.WriteLine("There is a problem , please try later");
+                    }
+                    else
+                    {
+                        throw new FileNotFoundException(filepath + " is not present",ex);
+
+                    }
+                }
+            }
+            catch (Exception excep)
+            {
+                Console.WriteLine("Current Exception = {0}", excep.GetType().Name);
+                if (excep.InnerException != null)
+                {
+                    Console.WriteLine("inner Exception = {0}", excep.InnerException.GetType().Name);
+                }
+            }
+        }
+    }
+}
