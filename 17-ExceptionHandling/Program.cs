b2480ec32a1f6131ce5c4d929ec2c5703c0e4b1e
afshan ramzan <afshan_ramzan@hotmail.com>
2020-03-20 16:30:04 +0500

    Exception handling

---
+++ 17-ExceptionHandling/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace _17_ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader=null;
+            try
+            {
+                reader = new StreamReader(@"E:\sample\files.txt");
+                Console.WriteLine(reader.ReadToEnd());
+                reader.Close();
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine("Please check if the file {0} ",ex.FileName);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine(ex.StackTrace);
+            }
+            catch(Exception ex) //general exception at bottom catch ...it its the practical example
+            //of inheritance, Exception is base class and File not found is special class
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+                Console.WriteLine("finally block excute.");
+            }
+        }
+    }
+}
