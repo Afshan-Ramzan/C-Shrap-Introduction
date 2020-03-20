8c7127af34c262fbcb1cc4f9a91d1811ad4301de
afshan ramzan <afshan_ramzan@hotmail.com>
2020-03-20 17:34:47 +0500

    write your own exception

---
+++ 19-CustomException/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+namespace _19_CustomException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new UserAlreadylogedinException("User Already Logged in, no Duplicate session allowed");
+            }
+            catch (UserAlreadylogedinException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
+[Serializable]
+public class UserAlreadylogedinException:Exception
+{
+    public UserAlreadylogedinException() : base()
+    {
+
+    }
+    public UserAlreadylogedinException(string message):base(message)
+    {
+        
+    }
+    public UserAlreadylogedinException(string message,Exception innerException) : base(message,innerException)
+    {
+
+    }
+    public UserAlreadylogedinException(SerializationInfo info,StreamingContext context):base(info,context)
+    {
+
+    }
+
+}
