fc517e519328300426fcd8de2de4b0d4b0e0ee46
afshan ramzan <afshan_ramzan@hotmail.com>
2020-03-20 13:08:28 +0500

    Array in C# and their working

---
+++ 4-Array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _4_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] EvenNumber = new int[3];
+            EvenNumber[0] = 0;
+            EvenNumber[1] = 2;
+            //EvenNumber[2] = 4;
+            Console.WriteLine(EvenNumber[2]);
+
+            SampleClass b;
+        }
+
+
+    }
+    /// <summary>
+    /// hello this is checking
+    /// </summary>
+    public class SampleClass
+    {
+
+    }
+}
