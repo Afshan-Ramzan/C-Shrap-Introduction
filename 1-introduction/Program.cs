9af42aec912d1f27b94e518263a5a28dad2289e0
afshan ramzan <afshan_ramzan@hotmail.com>
2020-03-20 13:02:47 +0500

    Introduction to C# Basics

---
+++ 1-introduction/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* string firstName, lastName;
+            Console.WriteLine(" enter your first name :");
+            firstName= Console.ReadLine();
+            Console.WriteLine(" enter your last name :");
+            lastName = Console.ReadLine();
+            Console.WriteLine("hello {0} {1}", firstName, lastName);
+            Console.WriteLine("Hello "+firstName+" "+lastName); */
+
+            //data types;
+            bool b = true;
+            float a = 1.23f;
+            if (b==true)
+            {
+                Console.WriteLine("value of a is {0}", a);
+            }
+
+            int i = 0;
+            Console.WriteLine(int.MaxValue);
+            //escape sequences;
+            Console.WriteLine("\"");
+            //verbatim literal
+            string path = @"C:\user\afshan\abc.doc";
+            Console.WriteLine(path);
+        }
+    }
+}
