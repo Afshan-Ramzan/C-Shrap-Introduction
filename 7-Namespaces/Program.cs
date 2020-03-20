080a108c1ee93a86898a5df7e18dc34467b5cfbc
afshan ramzan <afshan_ramzan@hotmail.com>
2020-03-20 13:12:22 +0500

    Namespaces and how to work with reference

---
+++ 7-Namespaces/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ProjectA.TeamA;
+using PATB = ProjectA.TeamB;
+namespace _7_Namespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProjectA.TeamA.ClassA.print();
+            ProjectA.TeamB.ClassA.print();
+            PATB.ClassA.print();
+            ClassA.print();
+
+        }
+    }
+
+
+}
