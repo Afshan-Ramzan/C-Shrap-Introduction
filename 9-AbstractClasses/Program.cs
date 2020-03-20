5b8f11bfbbfad3bde87ec4469290edb5300db27b
afshan ramzan <afshan_ramzan@hotmail.com>
2020-03-20 13:16:29 +0500

    Abstract classes and how they work

---
+++ 9-AbstractClasses/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _9_AbstractClasses
+
+{
+    class Program : Customer
+
+
+    {
+
+        public override void print()
+        {
+            Console.WriteLine("Print Method");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Customer c = new Program();
+            c.print();
+        }
+    }
+
+    //abstract class are incomplete it is only be used as base class for inheritance.
+
+    public abstract class Customer
+    {
+
+        public abstract void print();
+
+    }
+    /*
+     * difference b/w abstract class and interface
+     * abstract class have implementation of some of it methods but interafce class
+     * can not have implementation for it methods
+     * abstract class have flieds but interface cnt
+     * interface class inherit from another interface only
+     * but abstract class inherit from interface and another abstract class
+     * class inheritance from multiple interface but not from multiple class.
+     */
+}
