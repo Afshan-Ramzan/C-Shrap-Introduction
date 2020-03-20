db45c0c0a569f0b8dc2b6409c1bdf485ace7350f
afshan ramzan <afshan_ramzan@hotmail.com>
2020-03-20 13:24:17 +0500

    how to call interface Explicitly

---
+++ 13-ExplicitInterfacecall/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _13_ExplicitInterfacecall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            check c = new check();
+            ((I2)c).Interafcemethod();
+
+            // default implementation
+            c.Interafcemethod();
+        }
+    }
+
+
+    interface I1
+    {
+        void Interafcemethod();
+
+    }
+
+    interface I2
+    {
+        void Interafcemethod();
+    }
+
+    public class check : I1, I2
+    {
+        //default implementation
+        public void Interafcemethod()
+        {
+            Console.WriteLine("interface 1 method");
+        }
+
+        //explicit implementation
+        void I2.Interafcemethod()
+        {
+            Console.WriteLine("interface 2 method");
+        }
+    }
+}
