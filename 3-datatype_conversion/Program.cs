0d9785566d32e921b6dbefd0600c71c2cfd5e049
afshan ramzan <afshan_ramzan@hotmail.com>
2020-03-20 13:07:18 +0500

    Data types in C# and thier Conversion

---
+++ 3-datatype_conversion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace datatype_converison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float f = 123.45f;
+            int i = Convert.ToInt32(f);
+            Console.WriteLine(i);
+
+            string strNumber = "100tg";
+            //int Num = int.Parse(strNumber);
+            int Result = 0;
+            bool isConversion = int.TryParse(strNumber, out Result);
+            if (isConversion)
+            {
+                Console.WriteLine(Result);
+            }
+            else
+            {
+                Console.WriteLine("please enter valid number");
+            }
+
+        }
+    }
+}
