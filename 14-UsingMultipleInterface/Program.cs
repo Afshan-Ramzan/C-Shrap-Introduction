bb3d9a805421c49c31bf721a1511e930128230b8
afshan ramzan <afshan_ramzan@hotmail.com>
2020-03-20 13:33:40 +0500

    use of Multiple Interfaces

---
+++ 14-UsingMultipleInterface/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _14_UsingMultipleInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            AB ab = new AB();
+            ab.Amethod();
+        }
+    }
+}
+interface IA
+{
+    void Amethod();
+}
+interface IB
+{
+    void Bmethod();
+}
+
+public class A : IA
+{
+    public void Amethod()
+    {
+        Console.WriteLine("A");
+
+    }
+
+}
+
+public class B : IB
+{
+    public void Bmethod()
+    {
+        Console.WriteLine("B");
+    }
+
+}
+
+public class AB : IA, IB
+{
+    A a = new A();
+    B b = new B();
+    public void Amethod()
+    {
+        a.Amethod();
+    }
+    public void Bmethod()
+    {
+        b.Bmethod();
+    }
+}
