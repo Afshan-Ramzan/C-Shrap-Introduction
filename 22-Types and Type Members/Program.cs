4d88cd4f2a9c5fdd2080ea888873171e447f58d1
afshan ramzan <afshan_ramzan@hotmail.com>
2020-03-21 16:12:13 +0500

    type & type members in c#

---
+++ 22-Types and Type Members/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _22_Types_and_Type_Members
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
