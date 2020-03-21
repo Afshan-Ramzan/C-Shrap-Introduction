7cecd0ab149705e5b75c4c78655aed306b4d9fb0
afshan ramzan <afshan_ramzan@hotmail.com>
2020-03-21 16:41:05 +0500

    Internal and Protected Access Modifiers

---
+++ Assembly1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Assembly1
+{
+    public class Assembly1Class1
+    {
+         internal int ID = 101;
+
+    }
+
+    public class Assembly1Class2
+    {
+        public void SampleMethod()
+        {
+            Assembly1Class1 a1 = new Assembly1Class1();
+            Console.WriteLine(a1.ID);
+        }
+
+    }
+
+    public class Assembly1Class3
+    {
+        protected internal int ID=111;
+    }
+}
