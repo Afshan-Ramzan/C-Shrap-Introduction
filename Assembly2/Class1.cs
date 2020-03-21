4bb2481120d13c2451370e8b0f6ff7beedac728a
afshan ramzan <afshan_ramzan@hotmail.com>
2020-03-21 16:53:08 +0500

    Internal and Protected Access Modifiers + Access Modifier for type.

---
+++ Assembly2/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Assembly1;
+namespace Assembly2
+{
+    public class Assembly2Class1 : Assembly1Class3
+    {
+        public void Print()
+        {
+            Assembly1Class1 a1 = new Assembly1Class1();
+            //a1.ID = 98;
+            Assembly1Class3 a3 = new Assembly1Class3();
+            base.ID=888;
+            Assembly2Class1 a2 = new Assembly2Class1();
+            a2.ID = 567;
+        }
+    }
+    //types can only have internal or public access modifiers.
+    // by default it is internal.
+    //type members by default private.
+    public class Assembly2Class2
+    {
+    }
+}
